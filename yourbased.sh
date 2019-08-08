8ac8e772d05fe47178e66c29ce455c2538b3cf61
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 12:10:27 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata
+apt-get update && apt-get install -y tzdata libmysqlclient-dev
 gem install bundler -v 2.0.1
 # install
 bundle install --jobs=4 --retry=3 --deployment
